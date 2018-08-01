bce6a38acb2f23063dce4c6fba960fc42f3127b5
Ashwin Sankaralingam <ashwinoscar@gmail.com>
2018-08-01 11:45:17 -0600

    Update predict.py

--- predict.py
+++ predict.py
@@ -4,6 +4,7 @@ from keras.models import Sequential
 from keras.preprocessing.image import ImageDataGenerator
 from keras.models import model_from_json
 from keras.preprocessing import image
+import numpy as np
 
 target_size = (64, 64)
 target_dims = (64, 64, 3) # add channel for RGB
@@ -23,13 +24,14 @@ class ASLPredictor:
         print("Loaded model from disk")
     
     def predict(self):
-        image_dir = "input/asl_alphabet_test"
-        # img = image.load_img(img_dir, target_size=target_size)
-        data_augmentor = ImageDataGenerator(samplewise_center=True, 
-                                    samplewise_std_normalization=True, 
-                                    validation_split=val_frac)
-        test_generator = data_augmentor.flow_from_directory(image_dir, target_size=target_size, batch_size=batch_size, shuffle=False,class_mode=None)
-        probabilities = self.model.predict_generator(test_generator)
+        image_dir = "input/asl_alphabet_test/A_test.jpg"
+        img = image.load_img(img_dir, target_size=target_size)
+        img = np.reshape(np.array(img),target_dims)
+        # data_augmentor = ImageDataGenerator(samplewise_center=True, 
+        #                             samplewise_std_normalization=True, 
+        #                             validation_split=val_frac)
+        # test_generator = data_augmentor.flow_from_directory(image_dir, target_size=target_size, batch_size=batch_size, shuffle=False,class_mode=None)
+        probabilities = self.model.predict_classes(img)
     
 
 c = ASLPredictor()
