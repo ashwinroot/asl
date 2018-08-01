354fe0224d3c7f713b47be8f68956b0b423eb1f0
Ashwin Sankaralingam <ashwinoscar@gmail.com>
2018-08-01 11:20:35 -0600

    predict test

---
+++ predict.py
@@ -0,0 +1,33 @@
+import h5py
+from keras.layers import Conv2D, Dense, Dropout, Flatten
+from keras.models import Sequential
+from keras.preprocessing.image import ImageDataGenerator
+
+target_size = (64, 64)
+target_dims = (64, 64, 3) # add channel for RGB
+n_classes = 29
+val_frac = 0.1
+batch_size = 64
+
+class ASLPredictor:
+    def __init__(self):
+        # load json and create model
+        json_file = open('model.json', 'r')
+        loaded_model_json = json_file.read()
+        json_file.close()
+        self.model = model_from_json(loaded_model_json)
+        # load weights into new model
+        self.model.load_weights("model.h5")
+        print("Loaded model from disk")
+    
+    def predict(self):
+        image_dir = "input/asl_alphabet_test"
+        data_augmentor = ImageDataGenerator(samplewise_center=True, 
+                                    samplewise_std_normalization=True, 
+                                    validation_split=val_frac)
+        test_generator = data_augmentor.flow_from_directory(image_dir, target_size=target_size, batch_size=batch_size, shuffle=False)
+        probabilities = self.model.predict_generator(test_generator)
+    
+
+c = ASLPredictor()
+c.predict()
