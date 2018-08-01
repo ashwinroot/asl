459296a7a9f03a62caf3550c3db4970d3252c56f
Ashwin Sankaralingam <ashwinoscar@gmail.com>
2018-08-01 15:50:36 -0600

    Add files via upload

---
+++ test_image.py
@@ -0,0 +1,55 @@
+import h5py
+from keras.layers import Conv2D, Dense, Dropout, Flatten
+from keras.models import Sequential
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import model_from_json
+from keras.preprocessing import image
+import numpy as np
+
+data_dir = "input/asl_alphabet_train"
+target_size = (64, 64)
+target_dims = (64, 64, 3) # add channel for RGB
+n_classes = 29
+val_frac = 0.1
+batch_size = 64
+
+class ASLPredictor:
+    def __init__(self):
+        # load json and create model
+        json_file = open('mode.json', 'r')
+        loaded_model_json = json_file.read()
+        json_file.close()
+        self.model = model_from_json(loaded_model_json)
+        # load weights into new model
+        self.model.load_weights("model.h5")
+        self.model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=["accuracy"])
+        print("Loaded model from disk")
+    
+    def predict(self):
+        image_dir = "input/asl_alphabet_test/test/A_test.jpg "
+        # img = image.load_img(image_dir, target_size=target_size)
+        # test_datagen = ImageDataGenerator(rescale=1./255)
+        # test_generator = test_datagen.flow_from_directory(test_dir,target_size=target_size,color_mode="rgb",
+        # shuffle = False,
+        # class_mode='categorical',
+        # batch_size=1)
+        # data_augmentor = ImageDataGenerator()
+        # train_generator = data_augmentor.flow_from_directory(data_dir, target_size=target_size, batch_size=batch_size, shuffle=True)
+        # label_map = (train_generator.class_indices) 
+        # print("Labels are ")
+        # print(label_map)
+        
+        # test_generator = data_augmentor.flow_from_directory(image_dir, target_size=target_size, batch_size=batch_size, shuffle=False,class_mode='categorical')
+        # filenames = test_generator.filenames
+        # nb_samples = len(filenames) 
+        # print("testing : " + str(nb_samples))  
+        img = image.load_img(image_dir,target_size=(64,64,3))  
+        img = np.asarray(np.resize(img,(64,64,3)))
+        probabilities = self.model.predict(img)
+        print(probabilities)
+        y_classes = probabilities.argmax(axis=-1)
+        print(y_classes)
+   
+
+c = ASLPredictor()
+c.predict()
