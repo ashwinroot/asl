d58531ad0e72d6d865890d600efd6efa964dc81f
Ubuntu <ubuntu@ip-172-31-48-130.ec2.internal>
2018-08-02 00:32:32 +0000

    new train method

---
+++ train2.py
@@ -0,0 +1,177 @@
+import keras
+from keras.layers import Dense, Dropout, Activation, Flatten, Conv2D, MaxPooling2D, Lambda, MaxPool2D, BatchNormalization
+from keras.utils import np_utils
+from keras.utils.np_utils import to_categorical
+from keras.preprocessing.image import ImageDataGenerator
+from keras import models, layers, optimizers
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix, accuracy_score
+from sklearn.utils import class_weight
+from keras.optimizers import SGD, RMSprop, Adam, Adagrad, Adadelta, RMSprop
+from keras.models import Sequential, model_from_json
+from keras.layers import Activation,Dense, Dropout, Flatten, Conv2D, MaxPool2D,MaxPooling2D,AveragePooling2D, BatchNormalization
+from keras.preprocessing.image import ImageDataGenerator
+from keras.callbacks import ReduceLROnPlateau, ModelCheckpoint
+from keras import backend as K
+from keras.applications.vgg16 import VGG16
+from keras.models import Model
+from keras.applications.inception_v3 import InceptionV3
+import os
+from glob import glob
+import matplotlib.pyplot as plt
+import random
+import cv2
+import pandas as pd
+import numpy as np
+import matplotlib.gridspec as gridspec
+import seaborn as sns
+import zlib
+import itertools
+import sklearn
+import itertools
+import scipy
+import skimage
+from skimage.transform import resize
+import csv
+#from tqdm import tqdm
+from sklearn import model_selection
+from sklearn.model_selection import train_test_split, learning_curve,KFold,cross_val_score,StratifiedKFold
+from sklearn.utils import class_weight
+from sklearn.metrics import confusion_matrix
+#from imblearn.over_sampling import RandomOverSampler
+#from imblearn.under_sampling import RandomUnderSampler
+#from keras.applications.mobilenet import MobileNet
+#from sklearn.metrics import roc_auc_score
+#from sklearn.metrics import roc_curve
+#from sklearn.metrics import auc
+#import warnings
+#warnings.filterwarnings("ignore")
+
+imageSize =64
+train_dir = "input/asl_alphabet_train/"
+test_dir =  "input/asl_alphabet_test/"
+
+def get_data(folder):
+    """
+    Load the data and labels from the given folder.
+    """
+    X = []
+    y = []
+    for folderName in os.listdir(folder):
+        if not folderName.startswith('.'):
+            if folderName in ['A']:
+                label = 0
+            elif folderName in ['B']:
+                label = 1
+            elif folderName in ['C']:
+                label = 2
+            elif folderName in ['D']:
+                label = 3
+            elif folderName in ['E']:
+                label = 4
+            elif folderName in ['F']:
+                label = 5
+            elif folderName in ['G']:
+                label = 6
+            elif folderName in ['H']:
+                label = 7
+            elif folderName in ['I']:
+                label = 8
+            elif folderName in ['J']:
+                label = 9
+            elif folderName in ['K']:
+                label = 10
+            elif folderName in ['L']:
+                label = 11
+            elif folderName in ['M']:
+                label = 12
+            elif folderName in ['N']:
+                label = 13
+            elif folderName in ['O']:
+                label = 14
+            elif folderName in ['P']:
+                label = 15
+            elif folderName in ['Q']:
+                label = 16
+            elif folderName in ['R']:
+                label = 17
+            elif folderName in ['S']:
+                label = 18
+            elif folderName in ['T']:
+                label = 19
+            elif folderName in ['U']:
+                label = 20
+            elif folderName in ['V']:
+                label = 21
+            elif folderName in ['W']:
+                label = 22
+            elif folderName in ['X']:
+                label = 23
+            elif folderName in ['Y']:
+                label = 24
+            elif folderName in ['Z']:
+                label = 25
+            elif folderName in ['del']:
+                label = 26
+            elif folderName in ['nothing']:
+                label = 27
+            elif folderName in ['space']:
+                label = 28           
+            else:
+                label = 29
+            for image_filename in os.listdir(folder + folderName):
+                img_file = cv2.imread(folder + folderName + '/' + image_filename)
+                if img_file is not None:
+                    img_arr = np.resize(np.asarray(img_file), (imageSize, imageSize, 3))
+#                     img_arr = np.asarray(img_file)
+                    X.append(img_arr)
+                    y.append(label)
+    X = np.asarray(X)
+    y = np.asarray(y)
+    return X,y
+X_train, y_train = get_data(train_dir) 
+#X_test, y_test= get_data(test_dir) # Too few images
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X_train, y_train, test_size=0.2) 
+
+# Encode labels to hot vectors (ex : 2 -> [0,0,1,0,0,0,0,0,0,0])
+from keras.utils.np_utils import to_categorical
+y_trainHot = to_categorical(y_train, num_classes = 29)
+y_testHot = to_categorical(y_test, num_classes = 29)
+
+from sklearn.utils import shuffle
+X_train, y_trainHot = shuffle(X_train, y_trainHot, random_state=13)
+X_test, y_testHot = shuffle(X_test, y_testHot, random_state=13)
+X_train = X_train[:30000]
+X_test = X_test[:30000]
+y_trainHot = y_trainHot[:30000]
+y_testHot = y_testHot[:30000]
+
+target_dims =(64,64,3)
+my_model = Sequential()
+my_model.add(Conv2D(50, kernel_size=4, strides=1, activation='relu', input_shape=target_dims))
+my_model.add(Conv2D(50, kernel_size=4, strides=2, activation='relu'))
+my_model.add(Dropout(0.5))
+my_model.add(Conv2D(128, kernel_size=4, strides=1, activation='relu'))
+my_model.add(Conv2D(128, kernel_size=4, strides=2, activation='relu'))
+my_model.add(Dropout(0.5))
+my_model.add(Conv2D(256, kernel_size=4, strides=1, activation='relu'))
+my_model.add(Conv2D(256, kernel_size=4, strides=2, activation='relu'))
+my_model.add(Flatten())
+my_model.add(Dropout(0.5))
+my_model.add(Dense(512, activation='relu'))
+my_model.add(Dense(29, activation='softmax'))
+
+my_model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=["accuracy"])
+
+my_model.fit(X_train,y_trainHot, epochs=200, batch_size=1024,verbose=1)
+
+
+model = my_model
+model_json = model.to_json()
+with open("model_self.json", "w") as json_file:
+    json_file.write(model_json)
+# serialize weights to HDF5
+model.save_weights("model_self.h5")
+print("Saved model to disk")
