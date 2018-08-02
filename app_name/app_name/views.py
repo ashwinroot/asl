cc1b1ddec9de6ebe47665bc0322cea4e23543f3e
Ashwin Sankaralingam <ashwinoscar@gmail.com>
2018-08-01 20:13:41 -0600

    testing flask

--- app_name/app_name/views.py
+++ app_name/app_name/views.py
@@ -1,8 +1,26 @@
 from app_name import app
 
 from flask import render_template
+from flask import Flask, request, Response
+from ASLPredictor import ASLPredictor
+import cv2
+import numpy as np
+import json
 
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template("index.html")
+
+@app.route('/classify',methods=['POST'])
+def classify():
+    r = request
+    nparr = np.fromstring(r.data, np.uint8)
+    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    asl = ASLPredictor()
+    letter = asl.predict(img)
+    response = json.dumps({'message':letter})
+    return Response(response=response, status=200, mimetype="application/json")
+
+    
+
